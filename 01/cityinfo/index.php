0a77521bc7114cee128320106af8676ccf578e0b
贾丽鹏 <1094425279@qq.com>
2024-12-14 17:08:23 +0800

    first commit

---
+++ 01/cityinfo/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>易查供求信息网</title>
+    <style>
+        body {
+            background-image: url("Images/back.gif");
+        }
+    </style>
+</head>
+<body>
+<table width="920" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#ffffff">
+    <tr valign="top">
+        <td colspan="2"><?php include("top.php") ?></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+    </tr>
+</table>
+
+</body>
+</html>
+<?php
---
+++ 01/cityinfo/top.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+</head>
+<body>
+<div class="top">
+    <div>
+        <a href="">[发布信息]</a>
+        <a href="">[进入后台]</a>
+    </div>
+    <div>
+        <ul>
+            <li>
+                <a href="javascript:try{window.external.AddFavorite('http://localhost/cityinfo','52同城信息网')}catch(e){alert('请使用Ctrl + D收藏本站');}">加入收藏</a>
+            </li>
+            <li><a href="mailto:xor@cityinfo**.com">联系我们</a></li>
+        </ul>
+    </div>
+</div>
+<div>
+    <div class="logo"></div>
+    <div class="menu">
+        <ul>
+            <li><a href="">首&nbsp;页</a></li>
+            <li><a href="">招聘信息</a></li>
+            <li><a href="">培训信息</a></li>
+            <li><a href="">房屋信息</a></li>
+            <li><a href="">求购信息</a></li>
+            <li><a href="">求职信息</a></li>
+            <li><a href="">家教信息</a></li>
+            <li><a href="">车辆信息</a></li>
+            <li><a href="">出售信息</a></li>
+            <li><a href="">招商信息</a></li>
+            <li><a href="">寻物启示</a></li>
+        </ul>
+    </div>
+</div>
+<div class="pcard"></div>
+</body>
+</html>
