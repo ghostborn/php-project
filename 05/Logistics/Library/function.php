0884f7abe08248b6fe30a6b5420d37ee3ae58689
Galaxy-peng <1094425279@qq.com>
2025-01-06 22:31:25 +0800

    05 code commit

--- 05/Logistics/Library/function.php
+++ 05/Logistics/Library/function.php
@@ -5,11 +5,11 @@
  */
 function checkLogin(): bool
 {
-	session_start();
-	if (empty($_SESSION['user'])) {
-		return false;
-	}
-	return true;
+    session_start();
+    if (empty($_SESSION['user'])) {
+        return false;
+    }
+    return true;
 }
 
 /**
@@ -20,17 +20,125 @@ function checkLogin(): bool
  */
 function msg(int $type, $msg = null, $url = null)
 {
-	ob_start(); //开启输出缓存，确保在调用header函数之前没有任何输出
+    ob_start(); //开启输出缓存，确保在调用header函数之前没有任何输出
 
-	// 假设要跳转到的页面是 target.php，并且要传递参数 param1、param2和param3
-	$param1 = urlencode($type); //此函数便于将字符串编码并将其用于 URL 的请求部分，同时它还便于将变量传递给下一页。
-	$param2 = urlencode($msg);
-	$param3 = urlencode($url);
+    // 假设要跳转到的页面是 target.php，并且要传递参数 param1、param2和param3
+    $param1 = urlencode($type); //此函数便于将字符串编码并将其用于 URL 的请求部分，同时它还便于将变量传递给下一页。
+    $param2 = urlencode($msg);
+    $param3 = urlencode($url);
 
-	$targetUrl = "msg.php?type=" . $param1 . "&msg=" . $param2 . "&url=" . $param3;
-	// 进行页面跳转
-	// Location是HTTP响应头的一部分，告诉浏览器跳转到指定的url
-	header('Location:' . $targetUrl);
-	exit; //终止脚本的执行，避免后续代码的执行影响重定向操作
+    $targetUrl = "msg.php?type=" . $param1 . "&msg=" . $param2 . "&url=" . $param3;
+    // 进行页面跳转
+    // Location是HTTP响应头的一部分，告诉浏览器跳转到指定的url
+    header('Location:' . $targetUrl);
+    exit; //终止脚本的执行，避免后续代码的执行影响重定向操作
+}
+
+/**
+ * 获取当前url
+ * @return string
+ */
+function getUrl(): string
+{
+    $url = '';
+    $url .= $_SERVER['SERVER_PORT'] == 443 ? 'https://' : 'http://';
+    $url .= $_SERVER['HTTP_HOST'];
+    $url .= $_SERVER['REQUEST_URI'];
+    return $url;
+}
+
+
+/**
+ * 根据page生成url
+ * @param $page
+ * @param string $url
+ * @return string
+ */
+function pageUrl($page, string $url = '')
+{
+    $url = empty($url) ? getUrl() : $url;
+    //查询url中是否存在?
+    $pos = strpos($url, '?');
+    if ($pos === false) {
+        $url .= '?page=' . $page;
+    } else {
+        $queryString = substr($url, $pos + 1);
+        //解析querystring为数组
+        parse_str($queryString, $queryArr);
+        if (isset($queryArr['page'])) {
+            unset($queryArr['page']);
+        }
+        $queryArr['page'] = $page;
+
+        //将queryArr重新拼接成queryString
+        $queryStr = http_build_query($queryArr);
+        $url = substr($url, 0, $pos) . '?' . $queryStr;
+
+    }
+    return $url;
+}
+
+
+/**
+ * 分页显示
+ * @param int $total 数据总数
+ * @param int $currentPage 当前页
+ * @param int $pageSize 每页显示条数
+ * @param int $show 显示按钮数
+ * @return string
+ */
+function pages(int $total, int $currentPage, int $pageSize, int $show = 6)
+{
+    $pageStr = '';
+    //仅当总数大于每页显示条数 才进行分页处理
+    if ($total > $pageSize) {
+        // 总页数
+        $totalPage = ceil($total / $pageSize); //向上取整 获取总页数
+        // 对当前页进行处理
+        $currentPage = $currentPage > $totalPage ? $totalPage : $currentPage;
+        // 分页起始页
+        $from = max(1, ($currentPage - intval($show / 2)));
+        // 分页结束页
+        $to = $from + $show - 1;
+
+        $pageStr .= '<ul class="pagination">';
+
+        //仅当 当前页大于1的时候 存在 首页和上一页按钮
+        if ($currentPage > 1) {
+            $pageStr .= "<li><a href='" . pageUrl(1) . "'>首页</a></li>";
+            $pageStr .= "<li><a href='" . pageUrl($currentPage - 1) . "'>上一页</a></li>";
+        }
+
+        // 当结束页大于总页
+        if ($to > $totalPage) {
+            $to = $totalPage;
+            $from = max(1, $to - $show + 1);
+        }
+
+        if ($from > 1) {
+            $pageStr .= '<li><a>...</a></li>';
+        }
+
+        for ($i = $from; $i <= $to; $i++) {
+            if ($i != $currentPage) {
+                $pageStr .= "<li><a href='" . pageUrl($i) . "'>{$i}</a></li>";
+            } else {
+                $pageStr .= "<li><span class='curr-page'>{$i}</span></li>";
+            }
+        }
+
+        if ($to < $totalPage) {
+            $pageStr .= '<li><a>...</a></li>';
+        }
+
+        if ($currentPage < $totalPage) {
+            $pageStr .= "<li><a href='" . pageUrl($currentPage + 1) . "'>下一页</a></li>";
+            $pageStr .= "<li><a href='" . pageUrl($totalPage) . "'>尾页</a></li>";
+        }
+        $pageStr .= '</ul>';
+
+
+    }
+    return $pageStr;
 
 }
--- 05/Logistics/index.php
+++ 05/Logistics/index.php
@@ -4,21 +4,69 @@ include_once('Library/function.php');
 
 //判断是否登录
 if (!checkLogin()) {
-	msg(2, '请先登录', 'login.php');
+    msg(2, '请先登录', 'login.php');
 }
+// 获取查询条件
+$start = $_GET['start'] ?? "";
+$end = $_GET['end'] ?? "";
+
+// 分页设置
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1; //检查page参数
+$pageSize = 5; //每页显示条数
+$show = 6;//按钮显示数量
+$offset = ($page - 1) * $pageSize;  //设置起点
+
+// 获取分页数据
+$query_all = "select count(*) from tb_car where car_road like '%$start%' and car_road like '%$end%' ";
+$result = $pdo->prepare($query_all);
+$result->execute();
+$total = $result->fetchColumn();
+
+/** 筛选数据 **/
+$query = "SELECT tb_car.*,tb_car_log.car_log FROM tb_car LEFT JOIN tb_car_log ON tb_car_log.car_number = tb_car.car_number";
+$query .= "where car_road like '%$start%' and car_road like '%$end%' order by id desc limit {$offset},{$pageSize}";
+$res = $pdo->prepare($query);
+$res->execute();
+
 ?>
 
 <?php include_once('View/header.html') ?>
 
 <div class="container-fluid">
     <!--顶部导航-->
-	<?php include_once('View/nav.html') ?>
+    <?php include_once('View/nav.html') ?>
     <!--主区域开始-->
     <div class="row" style="margin-top:70px">
         <!--左侧菜单-->
-	    <?php include('View/menu.html') ?>
+        <?php include('View/menu.html') ?>
         <!--右侧主区域开始-->
         <div class="main-right  col-md-10 col-md-offset-2">
+            <div class="col-md-12">
+                <div class="panel panel-default ">
+                    <div class="panel-heading text-center">
+                        <form class="form-inline" action="index.php" method="get">
+                            <div class="form-group">
+                                <label for="start">路线：</label>
+                                <input type="text" class="form-control" id="start" name="start"
+                                       value=<?php echo $start ?>>
+                            </div>
+                            <div class="form-group">
+                                <label for="end">至</label>
+                                <input type="text" class="form-control" id="end" name="end" value=<?php echo $end ?>>
+                            </div>
+                            <button class="btn btn-info"><i class="glyphicon glyphicon-search"></i></button>
+                        </form>
+                    </div>
+                    <div class="panel-body">
+                        <!--路线不存在情况-->
+                        <?php
+                        if (empty($))
+                        ?>
+                    </div>
+
+
+                </div>
+            </div>
 
         </div>
 
